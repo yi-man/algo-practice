3e0406eddd2ac8012737ae52a7a59dec94123a4e
许鑫 <xin.xu1@yitu-inc.com>
2020-04-24 17:33:26 +0800

    feat: 回文字符

--- src/dynamic/huiwen.ts
+++ src/dynamic/huiwen.ts
@@ -1,17 +1,28 @@
-public String longestPalindrome(String s) {
-  int length = s.length();
-  boolean[][] P = new boolean[length][length];
-  int maxLen = 0;
-  String maxPal = "";
-  for (int len = 1; len <= length; len++) //遍历所有的长度
-      for (int start = 0; start < length; start++) {
-          int end = start + len - 1;
-          if (end >= length) //下标已经越界，结束本次循环
-              break;
-          P[start][end] = (len == 1 || len == 2 || P[start + 1][end - 1]) && s.charAt(start) == s.charAt(end); //长度为 1 和 2 的单独判断下
-          if (P[start][end] && len > maxLen) {
-              maxPal = s.substring(start, end + 1);
-          }
-      }
+function longestPalindrome(s: string) {
+  const length = s.length;
+  const P = new Array();
+  
+  for(let i = 0; i < length; i++) {
+    P.push(new Array(length).fill(0))
+  }
+
+  const maxLen = 0;
+  let maxPal = "";
+
+  for (let len = 1; len <= length; len++) //遍历所有的长度
+    for (let start = 0; start < length; start++) {
+    let end = start + len - 1;
+    if (end >= length) //下标已经越界，结束本次循环
+        break;
+
+    //长度为 1 和 2 的单独判断下
+    if ((len == 1 || len == 2 || P[start + 1][end - 1]) && s.charAt(start) === s.charAt(end)) {
+    P[start][end] = 1;
+    }
+    
+    if (P[start][end] && len > maxLen) {
+        maxPal = s.substring(start, end + 1);
+    }
+  }
   return maxPal;
 }
